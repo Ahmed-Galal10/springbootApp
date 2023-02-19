c2d87723b00776402f5ccbe788288ce57e6d071f
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-19 21:12:04 +0200

    Implement ExceptionHandler for UserNotFoundException

--- src/main/java/com/learn/restful/utils/exception/handler/CustomizedExceptionHandler.java
+++ src/main/java/com/learn/restful/utils/exception/handler/CustomizedExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.learn.restful.utils.exception.handler;
 
 import com.learn.restful.utils.exception.ExceptionDetails;
+import com.learn.restful.utils.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,10 +16,17 @@ import java.util.Date;
 public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleAllException(Exception ex) {
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex) {
         ExceptionDetails exceptionDetails = this.getExceptionDetails(ex, HttpStatus.INTERNAL_SERVER_ERROR);
 
         return new ResponseEntity<>(exceptionDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex) {
+        ExceptionDetails exceptionDetails = this.getExceptionDetails(ex, HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
     }
 
     private ExceptionDetails getExceptionDetails(Exception ex, HttpStatus status) {
